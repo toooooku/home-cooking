f370ee93f3ca26854ab72ae1ba9760c0f6769241
toooooku <79389139+toooooku@users.noreply.github.com>
2021-04-07 14:50:59 +0900

    バリデーション実装

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :image, presence: true
 end
