bafa02f2d18da81458e61990f19f80ede22db201
toooooku <79389139+toooooku@users.noreply.github.com>
2021-04-07 18:20:20 +0900

    画像表示の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,26 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
-
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        redirect_to root_path
+      else
+        render :new
+      end  
+  end
 
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :image).merge(user_id: current_usre.id)
+    params.require(:recipe).permit(:title, :cooking_recipe, :image).merge(user_id: current_user.id)
+  end
 end
+
