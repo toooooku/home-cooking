e5994b0c3841b954ee32758ec1df5b999e3f97d9
toooooku <79389139+toooooku@users.noreply.github.com>
2021-04-07 15:46:40 +0900

    Active Storageの導入

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,12 @@
 class RecipesController < ApplicationController
   def index
   end
+
+
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :image).merge(user_id: current_usre.id)
 end
